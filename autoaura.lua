8fd4d14c92f27f5c4a51e2a5af289e9c9f27f3af
CDawg <clintellyson@hotmail.com>
2020-11-21 20:02:15 -0800

    Removed old terminations not used in LUA

--- autoaura.lua
+++ autoaura.lua
@@ -18,41 +18,28 @@ https://www.wowinterface.com/forums/showthread.php?t=40444
 -- notes: if unitid ~= "player" then return end
 ]==]--
 
-app_name = "AutoAura"
-app_global = "|cffDA70D6" .. app_name .. ": |r"
-app_version = "1.0.1b"
-app_width = 300
-app_height = 340
-app_content_y = 10
+app_global = "|cffDA70D6AutoAura: |r"
+app_version = "1.2"
 
-DEFAULT_CHAT_FRAME:AddMessage (app_global .. "Initializing v" .. app_version .. "...");
+DEFAULT_CHAT_FRAME:AddMessage (app_global .. "Initializing v" .. app_version .. "...")
 
-function getPlayerInformation()
+local function getPlayerInformation()
   player_name = UnitName("player")
   return player_name
 end
 playerName = getPlayerInformation() .. "-" .. GetRealmName()
 
-function CancelPlayerBuff(buffName, buffIndex)
+local function CancelPlayerBuff(buffName, buffIndex)
   local i = 0
   if (UnitAffectingCombat('player')) then
-    DEFAULT_CHAT_FRAME:AddMessage (app_global .. "Unable to remove [" .. buffName .. "] during combat!");
+    DEFAULT_CHAT_FRAME:AddMessage (app_global .. "Unable to remove [" .. buffName .. "] during combat!")
   else
-    -- CancelUnitBuff("player", buffIndex);
-    if (ddNotify.text:GetText() == "Notify Me") then
-      DEFAULT_CHAT_FRAME:AddMessage (app_global .. buffName .. " removed!");
-    elseif (ddNotify.text:GetText() == "Notify Raid") then
-      SendChatMessage(app_name .. ": " .. buffName .. " removed!", "RAID");
-    end
-    CancelUnitBuff("player", buffIndex);
+    DEFAULT_CHAT_FRAME:AddMessage (app_global .. buffName .. " removed!")
+    CancelUnitBuff("player", buffIndex)
   end
 end
 
-function chat_message(msg, channel)
-  SendChatMessage(app_global .. msg, channel);
-end
-
-auraTimer = 1;
+auraTimer = 1
 function handleAuras()
   local total = 1
 
@@ -60,49 +47,49 @@ function handleAuras()
     while UnitBuff("player", total) do
         local buff = UnitBuff("player", total)
 
-        local localBuff = "Blessing of Salvation";
+        local localBuff = "Blessing of Salvation"
         if (checkbox["ARSalv"]:GetChecked()) then
           if string.find(buff, localBuff) then
             CancelPlayerBuff(localBuff, total)
           end
         end
-        local localBuff = "Blessing of Might";
+        local localBuff = "Blessing of Might"
         if (checkbox["ARMight"]:GetChecked()) then
           if string.find(buff, localBuff) then
             CancelPlayerBuff(localBuff, total)
           end
         end
-        local localBuff = "Blessing of Sanctuary";
+        local localBuff = "Blessing of Sanctuary"
         if (checkbox["ARSanc"]:GetChecked()) then
           if string.find(buff, localBuff) then
             CancelPlayerBuff(localBuff, total)
           end
         end
-        local localBuff = "Arcane Brilliance";
+        local localBuff = "Arcane Brilliance"
         if (checkbox["ARInt"]:GetChecked()) then
           if string.find(buff, localBuff) then
             CancelPlayerBuff(localBuff, total)
           end
         end
-        local localBuff = "Arcane Intellect";
+        local localBuff = "Arcane Intellect"
         if (checkbox["ARInt"]:GetChecked()) then
           if string.find(buff, localBuff) then
             CancelPlayerBuff(localBuff, total)
           end
         end
-        local localBuff = "Prayer of Spirit";
+        local localBuff = "Prayer of Spirit"
         if (checkbox["ARPoS"]:GetChecked()) then
           if string.find(buff, localBuff) then
             CancelPlayerBuff(localBuff, total)
           end
         end
-        local localBuff = "Thorns";
+        local localBuff = "Thorns"
         if (checkbox["ARThorns"]:GetChecked()) then
           if string.find(buff, localBuff) then
             CancelPlayerBuff(localBuff, total)
           end
         end
-      -- DEFAULT_CHAT_FRAME:AddMessage(app_global .. buff .. " index:" .. total); -- DEBUG
+      -- DEFAULT_CHAT_FRAME:AddMessage(app_global .. buff .. " index:" .. total) -- DEBUG
       total = total + 1
     end
   end
@@ -152,55 +139,51 @@ end
 
 function setAutoAuraVars()
   if (AutoAura[playerName]["ARSalv"]) then
-    checkbox["ARSalv"]:SetChecked(true);
+    checkbox["ARSalv"]:SetChecked(true)
   end
   if (AutoAura[playerName]["ARMight"]) then
-    checkbox["ARMight"]:SetChecked(true);
+    checkbox["ARMight"]:SetChecked(true)
   end
   if (AutoAura[playerName]["ARSanc"]) then
-    checkbox["ARSanc"]:SetChecked(true);
+    checkbox["ARSanc"]:SetChecked(true)
   end
   if (AutoAura[playerName]["ARInt"]) then
-    checkbox["ARInt"]:SetChecked(true);
+    checkbox["ARInt"]:SetChecked(true)
   end
   if (AutoAura[playerName]["ARPoS"]) then
-    checkbox["ARPoS"]:SetChecked(true);
+    checkbox["ARPoS"]:SetChecked(true)
   end
   if (AutoAura[playerName]["ARThorns"]) then
-    checkbox["ARThorns"]:SetChecked(true);
-  end
-  if (AutoAura[playerName]["ARNotification"]) then
-     ddNotify.text:SetText(AutoAura[playerName]["ARNotification"])
+    checkbox["ARThorns"]:SetChecked(true)
   end
 end
 
 function saveAutoAuraVars()
-  AutoAura[playerName]["ARInt"] = checkbox["ARInt"]:GetChecked();
-  AutoAura[playerName]["ARSalv"] = checkbox["ARSalv"]:GetChecked();
-  AutoAura[playerName]["ARPoS"] = checkbox["ARPoS"]:GetChecked();
-  AutoAura[playerName]["ARThorns"] = checkbox["ARThorns"]:GetChecked();
-  AutoAura[playerName]["ARMight"] = checkbox["ARMight"]:GetChecked();
-  AutoAura[playerName]["ARSanc"] = checkbox["ARSanc"]:GetChecked();
-  AutoAura[playerName]["ARNotification"] = ddNotify.text:GetText()
+  AutoAura[playerName]["ARInt"] = checkbox["ARInt"]:GetChecked()
+  AutoAura[playerName]["ARSalv"] = checkbox["ARSalv"]:GetChecked()
+  AutoAura[playerName]["ARPoS"] = checkbox["ARPoS"]:GetChecked()
+  AutoAura[playerName]["ARThorns"] = checkbox["ARThorns"]:GetChecked()
+  AutoAura[playerName]["ARMight"] = checkbox["ARMight"]:GetChecked()
+  AutoAura[playerName]["ARSanc"] = checkbox["ARSanc"]:GetChecked()
 end
 
 raiderList = {}
 function getRaiders()
   for i = 1, GetNumGroupMembers() do
-    raiderList[i] = GetRaidRosterInfo(i);
+    raiderList[i] = GetRaidRosterInfo(i)
   end
 end
 
-local frameMain = CreateFrame("Frame", nil, UIParent, "BasicFrameTemplateWithInset");
-frameMain:SetWidth(app_width);
-frameMain:SetHeight(app_height);
-frameMain:SetPoint("CENTER", 0, 0);
-frameMain:SetMovable(true);
-frameMain.text = frameMain:CreateFontString(nil,"ARTWORK");
-frameMain.text:SetFont("Fonts\\ARIALN.ttf", 13, "OUTLINE");
-frameMain.text:SetPoint("TOPLEFT", frameMain, "TOPLEFT", 10, -5);
-frameMain.text:SetText("|cffFF7D0AAUTO AURA " .. app_version);
-frameMain:Hide();
+local frameMain = CreateFrame("Frame", nil, UIParent, "BasicFrameTemplateWithInset")
+frameMain:SetWidth(300)
+frameMain:SetHeight(320)
+frameMain:SetPoint("CENTER", 0, 0)
+frameMain:SetMovable(true)
+frameMain.text = frameMain:CreateFontString(nil,"ARTWORK")
+frameMain.text:SetFont("Fonts\\ARIALN.ttf", 13, "OUTLINE")
+frameMain.text:SetPoint("TOPLEFT", frameMain, "TOPLEFT", 10, -5)
+frameMain.text:SetText("|cffFF7D0AAUTO AURA " .. app_version)
+frameMain:Hide()
 
 local profileText = CreateFrame("Frame",nil, frameMain)
 profileText:SetWidth(100)
@@ -241,12 +224,12 @@ end
 
 checkbox = {}
 function checkItem(checkID, checkName, icon, posY)
-  local check_static = CreateFrame("CheckButton", nil, frameMain, "ChatConfigCheckButtonTemplate");
+  local check_static = CreateFrame("CheckButton", nil, frameMain, "ChatConfigCheckButtonTemplate")
   check_static:SetPoint("TOPLEFT", 30, posY)
-  check_static.text = check_static:CreateFontString(nil,"ARTWORK");
-  check_static.text:SetFont("Fonts\\ARIALN.ttf", 14, "OUTLINE");
-  check_static.text:SetPoint("TOPLEFT", check_static, "TOPLEFT", 50, -5);
-  check_static.text:SetText(checkName);
+  check_static.text = check_static:CreateFontString(nil,"ARTWORK")
+  check_static.text:SetFont("Fonts\\ARIALN.ttf", 14, "OUTLINE")
+  check_static.text:SetPoint("TOPLEFT", check_static, "TOPLEFT", 50, -5)
+  check_static.text:SetText(checkName)
   -- check_static.tooltip = checkID
   local licon = check_static:CreateTexture(nil, "BACKGROUND", nil, -6)
   licon:SetTexture(icon)
@@ -277,40 +260,6 @@ checkItem("ARInt",  "Intellect/Brilliance", "Interface/Icons/Spell_Holy_ArcaneIn
 checkItem("ARPoS",  "Prayer of Spirit", "Interface/Icons/Spell_Holy_PrayerofSpirit", -200)
 checkItem("ARThorns","Thorns", "Interface/Icons/Spell_Nature_Thorns", -220)
 
--- NOTIFICATIONS --
-local frameText = CreateFrame("Frame",nil, frameMain)
-frameText:SetWidth(100)
-frameText:SetHeight(30)
-frameText:SetPoint("TOPLEFT", 10, -130)
-frameText.text=frameText:CreateFontString(nil, "ARTWORK")
-frameText.text:SetFont("Fonts\\ARIALN.ttf", 14, "OUTLINE")
-frameText.text:SetPoint("TOPLEFT", 10, -130)
-frameText.text:SetText("Notifications:")
-
-local ddNotifyList={"Notify Me", "Notify Raid", "No Notifications"}
-local posY = -280
-ddNotify = CreateFrame("frame", nil, frameMain, "UIDropDownMenuTemplate")
-ddNotify:SetPoint("TOPLEFT", 20, posY);
-ddNotify.text = ddNotify:CreateFontString(nil,"ARTWORK");
-ddNotify.text:SetFont("Fonts\\ARIALN.ttf", 13, "OUTLINE");
-ddNotify.text:SetPoint("TOPLEFT", ddNotify, "TOPLEFT", 25, -8);
-ddNotify.text:SetText("Notify Me");
-ddNotify.onClick = function(self, checked)
-	-- print(app_global .. "Debug Notifications: " .. self.value)
-  ddNotify.text:SetText(self.value)
-  saveAutoAuraVars()
-end
-
-ddNotify.initialize = function(self, level)
-	local info = UIDropDownMenu_CreateInfo()
-  for ddKey, ddVal in pairs(ddNotifyList) do
-    info.text = ddVal
-  	info.value= ddVal
-  	info.func = self.onClick
-  	UIDropDownMenu_AddButton(info, level)
-  end
-end
-
 function openWindow()
   frameMain:Show()
 end
@@ -319,7 +268,7 @@ end
 self:RegisterEvent("PARTY_INVITE_REQUEST", "confirmPartyInvite")
 function MyAddon:confirmPartyInvite(info, sender)
   if ( MyAddon:someTestOfSenderThatYouMakeUp(sender) ) then
-    AcceptGroup();
+    AcceptGroup()
     self:RegisterEvent("PARTY_MEMBERS_CHANGED", "closePopup")
   end
 end
@@ -363,11 +312,11 @@ buttonMinimap:SetScript("OnDragStart", function()
     buttonMinimap:SetScript("OnUpdate", UpdateMapButton)
 end)
 buttonMinimap:SetScript("OnDragStop", function()
-    buttonMinimap:StopMovingOrSizing();
+    buttonMinimap:StopMovingOrSizing()
     buttonMinimap:SetScript("OnUpdate", nil)
-    UpdateMapButton();
+    UpdateMapButton()
 end)
-buttonMinimap:ClearAllPoints();
+buttonMinimap:ClearAllPoints()
 buttonMinimap:SetPoint("TOPLEFT", Minimap, "TOPLEFT", 62 - (80 * cos(myIconPos)),(80 * sin(myIconPos)) - 62)
 buttonMinimap:SetScript("OnClick", function()
   openWindow()
